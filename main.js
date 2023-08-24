ff5d8c9e11869fd931e496b6860cd789533a163c
unknown <cris.sandoval.cf@gmail.com>
2023-08-23 19:10:16 -0500

    js-cart-ok

--- main.js
+++ main.js
@@ -31,31 +31,84 @@ addBtn.addEventListener('click', () => {
 
     cartNotification.innerText = lastNumber;
     cartNotification.style.display = 'block';
-    priceModal.innerHTML = `$125 x ${lastNumber} <span>${lastNumber*125}.00</span>`
+    drawProductModal();
+    
 })
 
 // mostrar el modal del carrito 
 
 const cartBtn = document.querySelector('.header__cart')
 const cartModal = document.querySelector('.cart-modal')
+let productContainer = document.querySelector('.cart-modal__checkout-container')
 let priceModal = document.querySelector('.cart-modal__price')
- 
+
 cartBtn.addEventListener('click', () => {
-    cartModal.classList.toggle('show')
+    cartModal.classList.toggle('show');
+    if (lastNumber == 0) {
+        drawProductModal();
+    }
 })
 
 // borrar el carrito o limpiarlo 
-const deleteCartBtn = document.querySelector('.cart-modal__delete')
-const productContainer = document.querySelector('.cart-modal__checkout-container')
+function deleteProduct() {
+    const deleteCartBtn = document.querySelector('.cart-modal__delete')
 
-deleteCartBtn.addEventListener('click', ()=>{
-    productContainer.innerHTML = '<p class="empty" > Your cart is empty </p>'
-    lastNumber = 0
-    cartNotification.innerText = lastNumber
+    deleteCartBtn.addEventListener('click', () => {
+        productContainer.innerHTML = '<p class="empty" > Your cart is empty </p>'
+        lastNumber = 0
+        cartNotification.innerText = lastNumber
+    })
+
+}
+
+// cambiar imagenes cuando se presione btn flechas
+const imageContainer = document.querySelector('.gallery__image-container')
+const previusBtn = document.querySelector('.gallery__previus')
+const nextBtn = document.querySelector('.gallery__next')
+
+let indexImg = 1
+
+nextBtn.addEventListener('click', ()=>{
+    changeNextImgBtn(imageContainer)
+})
+previusBtn.addEventListener('click', ()=>{
+    changePreviusImgBtn(imageContainer)
 })
 
+// funciones 
+
+function drawProductModal() {
+    productContainer.innerHTML = `
+        <div class="cart-modal__details-container">
+          <img class="cart-modal__img" src="./images/image-product-1-thumbnail.jpg" alt="products1">
+          <div>
+            <p class="cart-modal__products">Autumn Limited Edition...</p>
+            <p class="cart-modal__price">$125 x ${lastNumber} <span>${lastNumber * 125}.00</span></p>
+          </div>
+          <img class="cart-modal__delete" src="./images/icon-delete.svg" alt="delete">
+        </div>
+        <button class="cart-modal__checkout">Checkout</button>
+        `
+        deleteProduct()
+}
+
+function changeNextImgBtn(containerImg){
+    if(indexImg == 4){
+        indexImg = 1
+    }else{
+        indexImg++
+    }
+    containerImg.style.backgroundImage = `url('../images/image-product-${indexImg}.jpg')`
+   
+}
 
 
-
-
-
+function changePreviusImgBtn(containerImg){
+    if(indexImg == 1){
+        indexImg = 4
+    }else{
+        indexImg--
+    }
+    containerImg.style.backgroundImage = `url('../images/image-product-${indexImg}.jpg')`
+   
+}
